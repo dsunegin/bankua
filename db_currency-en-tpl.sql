8ad6ce76239855427f8f76cea4a07e4cefae4299
Denis Sunegin <dsunegin@gmail.com>
2021-03-02 18:54:59 +0000

    Initial commit

--- db_currency-en-tpl.sql
+++ db_currency-en-tpl.sql
@@ -1,27 +1,3 @@
-CREATE DATABASE finance CHARACTER SET utf8 COLLATE utf8_general_ci;
-create user finance_user@localhost;
-set password for finance_user@localhost=password('psw_finance_user');
-grant all on finance.* to finance_user@localhost with grant option;
-
-use finance;
-
-CREATE TABLE bankua (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-code VARCHAR(7) NOT NULL,
-exchangedate DATE NOT NULL,
-rate float UNSIGNED
-);
-
-# ISO 4217 Currency Codes for different languages e.g, USD = United States dollar
-
-
-CREATE TABLE currency(
-id       INTEGER  NOT NULL PRIMARY KEY,
-country  VARCHAR(100) NOT NULL,
-currency VARCHAR(100) NOT NULL,
-code     VARCHAR(3) NOT NULL,
-lang VARCHAR(2) NOT NULL
-);
 
 ############### ENGLISH ###############
 
